a480e72c3e7e1c697064ea38a5049a580fe729e8
Suranga Thilakasiri <suranga@suranga-desktop.(none)>
2015-07-30 17:42:22 +0530

    Add StudentreportController file to the system

---
+++ app/Controller/StudentreportController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+App::uses('AppController', 'Controller');
+
+class StudentreportController extends AppController {
+
+
+public function index(){
+
+	$this->render('/student_report');
+}
+
+
+
+
+}
+
+?>
